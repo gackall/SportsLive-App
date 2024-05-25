e1ecb9aeab4ce729bf54ff7270319eb464bcad20
Gabriel Ackall <ackall2020@gmail.com>
2024-05-25 12:55:27 -0400

    added chat feature

--- server.py
+++ server.py
@@ -18,15 +18,18 @@ def get_score():
     ngames = len(game_data["data"])
     for i in range(ngames):
         team1 = game_data["data"][i]["home_team"]["full_name"]
+        abbrev1 = game_data["data"][i]["home_team"]["abbreviation"]
         team2 = game_data["data"][i]["visitor_team"]["full_name"]
+        abbrev2 = game_data["data"][i]["visitor_team"]["abbreviation"]
         score1 = game_data["data"][i]["home_team_score"]
         score2 = game_data["data"][i]["visitor_team_score"]
         logo1 = f'/static/logos/{team1}.png'
         logo2 = f'/static/logos/{team2}.png'
         status = game_data["data"][i]["status"]
+        chatcode = abbrev1 + abbrev2
         if len(status) > 8:
             status = "Not Started"
-        game = {"team1": team1, "score1": score1, "team2": team2, "score2": score2, "logo1": logo1, "logo2": logo2, "status": status}
+        game = {"team1": team1, "score1": score1, "team2": team2, "score2": score2, "logo1": logo1, "logo2": logo2, "status": status, "chatcode": chatcode}
         games_list.append(game)
 
     return render_template(
